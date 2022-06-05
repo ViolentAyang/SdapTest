c5b99a8d9735028eefba0ad643b11bcaf3d491c1
ViolentAyang <76544389+ViolentAyang@users.noreply.github.com>
2022-06-05 15:20:19 +0800

    Update ex7.java

--- ex7.java
+++ ex7.java
@@ -1,32 +1,16 @@
-import java.util.Scanner;
-public class ex7 {
-	 public static void main(String args[])
+public class Wanshu {
+ public static void main(String[] args)
+ {
+	 int s;
+	 for(int i=1;i<=1000;i++)
 	 {
-	  System.out.println("请输入字符串：");
-	  Scanner scan=new Scanner(System.in);
-	  String str=scan.next();
-	  String E1="[\u4e00-\u9fa5]";
-	  String E2="[a-zA-Z]";
-	  int countH=0;
-	  int countE=0;
-	  char[] arrChar=str.toCharArray();
-	  String[] arrStr=new String[arrChar.length];
-	  for (int i=0;i<arrChar.length ;i++ )
-	  {
-	   arrStr[i]=String.valueOf(arrChar[i]);
-	  }
-	  for (String i: arrStr )
-	  {
-	   if (i.matches(E1))
-	   {
-	    countH++;
-	   }
-	   if (i.matches(E2))
-	   {
-	    countE++;
-	   }
-	  }
-	  System.out.println("汉字的个数"+countH);
-	  System.out.println("字母的个数"+countE);
-	 }	
- } 
+		 s=0;
+		 for(int j=1;j<i;j++)
+			 if(i % j==0)
+				 s=s+j;
+			if(s==i)
+				System.out.print(i+" ");
+	 }
+	 System.out.println();
+ }
+}
